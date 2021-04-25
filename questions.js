88d7b94fb4be60c9146f0ffa2afe7d90e1ade9ab
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-25 23:51:47 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I was born ___ 2007.",
+        options:["in", "at", "on"],
+        answer: 0
+    },
+    {
+     q:"His birthday is ___ Monday.",
+        options:["at", "on", "in"],
+        answer: 1   
+    },
+    {
+        q:"I like ___ .",
+        options:["very much chocolate", "a lot chocolate", "chocolate very much"],
+        answer: 2
+    },
+    {
+        q:"Lisa is married to John. She is his ___ .",
+        options:["wife", "aunt", "niece"],
+        answer: 0
+    },
+    {
+        q:"I like apples ___ I don't like pears.",
+        options:["and", "but", "also"],
+        answer: 1
+    },
+    {
+        q:"She has good marks ___ she works hard.",
+        options:["and","but", "because"],
+        answer: 2
+    },
+    {
+        q:"Did you see Ann?",
+        options:["No, I didn't.", "No, I wasn't.", "No, I don't."],
+        answer: 0
+    },
+    {
+        q:"___ old are you? ",
+        options:["What", "How", "Who"],
+        answer: 1
+    },
+    {
+        q:"___ are you doing now?",
+        options:["Why", "When", "What"],
+        answer: 2
+    },
+    {
+        q:"I love her Instagram ___ .",
+        options:["stories", "storyes", "storys"],
+        answer: 0
+    },
+    {
+        q:"I love watching films. I ___ to the cinema.",
+        options:["go often", "often go", "goes often"],
+        answer: 1
+    },
+    {
+        q:"'Mum, the baby ___ !'",
+        options:["cry", "cries", "is crying"],
+        answer: 2
+    },
+    {
+        q:"Those pencils are ___ .",
+        options:["mine", "my", "me"],
+        answer: 0
+    },
+    {
+        q:"Mike ___ to school in the afternoon.",
+        options:["don't go", "doesn't go", "doesn't goes"],
+        answer: 1
+    },
+    {
+        q:"The twins ___ born in June.",
+        options:["be", "was", "were"],
+        answer: 2
+    },
+    {
+        q:"We ___ to the cinema last night.",
+        options:["went", "go", "are going"],
+        answer: 0
+    },
+    {
+        q:"It's ___ (9.45).",
+        options:["quarter past nine", "quarter to ten", "quarter to nine"],
+        answer: 1
+    },
+    {
+        q:"I'm from Poland, so I'm ___ .",
+        options:["Polanese", "Polac", "Polish"],
+        answer: 2
+    },
+    {
+        q:"Excuse me! ___ is this T-shirt?",
+        options:["How much", "How many", "How price"],
+        answer: 0
+    },
+    {
+        q:"I love Kate. She is so ___ .",
+        options:["annoying", "nice", "boring"],
+        answer: 1
+    }
+]
